8989fa40d101358ae15c64ec06e6aa7115b1a7ea
Nelson <contact.nelsonic@gmail.com>
2015-11-11 20:09:43 +0000

    adds basic string substitution test + code. passes

---
+++ test/keyword-highlighter.test.js
@@ -0,0 +1,17 @@
+var dir  = __dirname.split('/')[__dirname.split('/').length-1];
+var file = dir + __filename.replace(__dirname, '') + " > ";
+var test = require('tape');
+
+var h    = require('../lib/keyword-highlighter.js');
+
+// h.setStartTag();
+// h.setEndTag();
+
+
+test(file+'Basic Single String Substitution', function(t) {
+  var str = 'The Cat in the Hat Sat on the Mat.';
+  var res = h('Hat', str);
+  var expected = "The Cat in the <b class='highlight'>Hat</b> Sat on the Mat.";
+  t.equal(res, expected, 'The Word "Hat" is highlighted: '+res);
+  t.end();
+});
