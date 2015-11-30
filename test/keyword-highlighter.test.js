034441688c413149314db743078672acd7add00d
SimonLab <simon.labondance@gmail.com>
2015-11-30 22:42:52 +0000

    resolve the question/issue https://github.com/dwyl/search-result-keyword-highlighter/issues/13

--- test/keyword-highlighter.test.js
+++ test/keyword-highlighter.test.js
@@ -45,3 +45,11 @@ test(file+'Find keyword containing hyphen', function(t) {
   t.equal(result, expected, 'Highlight hyphenated word: '+result);
   t.end();
 });
+
+test(file+'Find keyword and delete unnecessary spaces', function(t) {
+  var text = 'Whaouu too many spaces here!';
+  var result = h('many            spaces', text);
+  var expected = "Whaouu too <b class='highlight'>many</b> <b class='highlight'>spaces</b> here!";
+  t.equal(result, expected, 'Highlight "spaces" word and delete unnecessary spaces: '+result);
+  t.end();
+});
